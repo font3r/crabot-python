3f2b5354fbc9e8e935b8a15428bce7825f49b1e4
fonter <serfin009@gmail.com>
2026-03-17 23:52:45 +0100

    chore: project cleanups

--- src/gateway_contracts.py
+++ src/gateway_contracts.py
@@ -5,73 +5,84 @@ import json
 
 
 class GatewayOpcode(IntEnum):
-	DISPATCH          = 0
-	HEARTBEAT         = 1
-	IDENTIFY          = 2
-	INVALID_SESSION   = 9
-	HELLO             = 10
-	HEARTBEAT_ACK     = 11
+    DISPATCH = 0
+    HEARTBEAT = 1
+    IDENTIFY = 2
+    INVALID_SESSION = 9
+    HELLO = 10
+    HEARTBEAT_ACK = 11
+
 
 @dataclass
 class GatewayPayload:
-	op: GatewayOpcode
-	data: Any = None
-	sequence_number: int | None = None
-	event_name: str | None = None
+    op: GatewayOpcode
+    data: Any = None
+    sequence_number: int | None = None
+    event_name: str | None = None
 
-	def to_json(self) -> str:
-		return json.dumps({"op": self.op, "d": self.data, "s": self.sequence_number, "t": self.event_name})
+    def to_json(self) -> str:
+        return json.dumps(
+            {
+                "op": self.op,
+                "d": self.data,
+                "s": self.sequence_number,
+                "t": self.event_name,
+            }
+        )
 
-	@staticmethod
-	def from_json(data: str) -> GatewayPayload:
-		raw = json.loads(data)
-		return GatewayPayload(
-			op=raw["op"],
-			data=raw.get("d"),
-			sequence_number=raw.get("s"),
-			event_name=raw.get("t")
-		)
+    @staticmethod
+    def from_json(data: str) -> GatewayPayload:
+        raw = json.loads(data)
+        return GatewayPayload(
+            op=raw["op"],
+            data=raw.get("d"),
+            sequence_number=raw.get("s"),
+            event_name=raw.get("t"),
+        )
+
 
 @dataclass
 class MessageEvent:
-	channel_id: str
-	content: str
-	author_username: str
+    channel_id: str
+    content: str
+    author_username: str
 
-	@staticmethod
-	def from_payload(d: dict) -> MessageEvent:
-		return MessageEvent(
-			channel_id=d["channel_id"],
-			content=d["content"],
-			author_username=d["author"]["username"]
-		)
+    @staticmethod
+    def from_payload(d: dict) -> MessageEvent:
+        return MessageEvent(
+            channel_id=d["channel_id"],
+            content=d["content"],
+            author_username=d["author"]["username"],
+        )
+
 
 @dataclass
 class ReadyEvent:
-	session_id: str
-	username: str
-	discriminator: str
+    session_id: str
+    username: str
+    discriminator: str
 
-	@staticmethod
-	def from_payload(d: dict) -> ReadyEvent:
-		return ReadyEvent(
-			session_id=d["session_id"],
-			username=d["user"]["username"],
-			discriminator=d["user"]["discriminator"]
-		)
+    @staticmethod
+    def from_payload(d: dict) -> ReadyEvent:
+        return ReadyEvent(
+            session_id=d["session_id"],
+            username=d["user"]["username"],
+            discriminator=d["user"]["discriminator"],
+        )
+
 
 @dataclass
 class IdentifyData:
-	token: str
-	intents: int
+    token: str
+    intents: int
 
-	def to_dict(self) -> dict:
-		return {
-			"token": self.token,
-			"intents": self.intents,
-			"properties": {
-				"os": "linux",
-				"browser": "crabot/0.0.1",
-				"device": "crabot/0.0.1"
-			}
-	  }
+    def to_dict(self) -> dict:
+        return {
+            "token": self.token,
+            "intents": self.intents,
+            "properties": {
+                "os": "linux",
+                "browser": "crabot/0.0.1",
+                "device": "crabot/0.0.1",
+            },
+        }
--- src/rest_client.py
+++ src/rest_client.py
@@ -7,25 +7,31 @@ DISCORD_API_GATEWAY: Final = "https://discord.com/api/v10/"
 
 _token: str
 
+
 def init_rest_client(token: str):
-	global _token
-	_token = token
+    global _token
+    _token = token
+
 
 def is_command(content: str) -> bool:
-	return content.startswith("!")
+    return content.startswith("!")
+
 
 async def handle_command(session: aiohttp.ClientSession, msg: MessageEvent):
-	if msg.content == "zjeb":
-		await send_message(session, msg.channel_id, "<@!269132306227265536> Sebastian Winek")
-	else:
-		await send_message(session, msg.channel_id, "invalid command")
+    if msg.content == "zjeb":
+        await send_message(session, msg.channel_id, "<@!269132306227265536>")
+    else:
+        await send_message(session, msg.channel_id, "invalid command")
+
 
 async def send_message(session: aiohttp.ClientSession, channel: str, messsage: str):
-	response = await session.post(f"{DISCORD_API_GATEWAY}/channels/{channel}/messages", 
-		headers={
-			"Authorization": f"Bot {_token}",
-			"User-Agent": "DiscordBot (crabot, 0.0.1)"
-		}, 
-		data={"content": messsage})
-	
-	response.raise_for_status()
+    response = await session.post(
+        f"{DISCORD_API_GATEWAY}/channels/{channel}/messages",
+        headers={
+            "Authorization": f"Bot {_token}",
+            "User-Agent": "DiscordBot (crabot, 0.0.1)",
+        },
+        data={"content": messsage},
+    )
+
+    response.raise_for_status()
