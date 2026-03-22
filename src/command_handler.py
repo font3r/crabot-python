035d7ce2296c9e035d1a76a8adb0e96408610164
fonter <serfin009@gmail.com>
2026-03-22 23:42:57 +0100

    feat: mention user support

--- src/command_handler.py
+++ src/command_handler.py
@@ -13,8 +13,11 @@ async def handle_command(client: DiscordRestClient, msg: MessageEvent):
   msg.content = msg.content.strip("!")
   
   if msg.content == "zjeb":
-    await client.send_message(msg.channel_id, "<@!269132306227265536>")
+    await client.send_message(msg.channel_id, mention(269132306227265536))
   elif msg.content == "aha":
     await client.send_message(msg.channel_id, "ok")
   else:
     await client.send_message(msg.channel_id, "invalid command")
+
+def mention(user_id: int) -> str:
+  return f"<@!{user_id}>"
